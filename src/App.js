c99c148d62997375443ad6cdb912cf12e4652ec1
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-30 20:07:09 +0530

    Added Base Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Cards from './components/Cards/Cards'
+import Chart from './components/Chart/Chart'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+
+
+import styles from './App.module.css'
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className={styles.container}>
+      <Cards />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
