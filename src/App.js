6ee0e289ab5ea354b0c54c126f96eaedd0827ae9
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-30 20:19:50 +0530

    Fetched Data From API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Cards from './components/Cards/Cards'
 import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 
+import { fetchData } from './api'
 
 import styles from './App.module.css'
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <Cards />
-      <Chart />
-      <CountryPicker />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+    this.setState({ data })
+  }
+
+
+  render() {
+    const { data } = this.state
+    return (
+      <div className={styles.container} >
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const URL = 'https://covid19.mathdro.id/api'
+
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(URL)
+        return {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        }
+    }
+    catch (err) {
+
+    }
+}
